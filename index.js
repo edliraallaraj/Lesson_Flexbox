de43f9426bfd3430eb0fe8c0a513a716082dd605
kristi2894 <kristikondil@gmail.com>
2022-07-02 10:57:34 +0200

    changes

---
+++ index.js
@@ -0,0 +1,6 @@
+// let firstStudent= document.getElementById("Mylist").firstElementChild
+// document.getElementById("first-el").innerHTML = firstStudent;
+
+let firstLanguage = document.getElementById("language").firstElementChild
+
+document.getElementById("language").innerHTML = firstLanguage;
