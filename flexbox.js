3d34c0bc1b473a4ea73aaf648e871ca435db0450
Loren Meta <loren_meta@yahoo.com>
2022-07-03 11:28:47 +0200

    canvas

--- flexbox.js
+++ flexbox.js
@@ -18,3 +18,17 @@
 
 //--------------------------------------------
 
+/*----------------------------Canvas */
+
+let limit = document.getElementById('myCanvas')
+let showCanvas = limit.getContext('2d')
+// showCanvas.moveTo(0,0);
+// showCanvas.lineTo(200,100);
+// showCanvas.stroke();
+
+//draw a circle in the middle of the ractangle
+
+showCanvas.beginPath()
+showCanvas.arc(95, 50, 40, 0, 2 * Math.PI)
+showCanvas.stroke();
+showCanvas.fill();
